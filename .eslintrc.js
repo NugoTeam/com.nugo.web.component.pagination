a252cb9ce1530eb0f12144446ef474b9d53eaedd
EGISBERTO V SILVA <egisberto@gmail.com>
2019-09-20 12:07:36 -0300

    Add eslint and upagrade packages

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    extends: [
+        "standard",
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    globals: {
+        Atomics: "readonly",
+        SharedArrayBuffer: "readonly"
+    },
+    parser: "babel-eslint",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module'
+    },
+    plugins: [
+        "react"
+    ],
+    rules: {
+        "react/display-name": [0, { "ignoreTranspilerName": false }]
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    }
+};
